7c1f2d9878afad353850e17aeea1360714c3685a
Vitalii Komisarenko <vitalii.komisarenko@yandex.ua>
2021-11-07 11:26:23 +0100

    SinParser: Minimal PoC: objects and few number types

---
+++ sin_parser.cpp
@@ -0,0 +1,273 @@
+#include <memory>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+#include "sin_value.h"
+#include "sin.h"
+
+class SinParser {
+public:
+    SinParser(const std::string &str);
+    Sin value;
+    std::string error;
+private:
+    size_t line_number = 0;
+    std::stringstream ss;
+
+    /**
+     * A wrapper around std::stringsteam::get() to implement line counter
+     */
+    int get_char();
+    std::set<char> whitespace =          {' ', '\t', '\n', '\r'};
+    std::set<char> whitespace_or_colon = {' ', '\t', '\n', '\r', ':'};
+    void skip_chars(const std::set<char> &chars);
+    void skip_whitespace();
+    std::string read_till_char(const std::set<char> &chars);
+    std::string read_number();
+
+    Sin read_sin_value();
+    std::string read_var_name();
+    std::string read_var_type();
+};
+
+int SinParser::get_char() {
+    if (ss.eof()) {
+        return EOF;
+    }
+    int ch = ss.get();
+    if (ch == '\n') {
+        line_number++;
+    }
+    return ch;
+}
+
+void SinParser::skip_chars(const std::set<char> &chars) {
+    while (!ss.eof()) {
+        char ch = ss.peek();
+        if (chars.contains(ch)) {
+            get_char();
+        }
+        else {
+            break;
+        }
+    }
+}
+
+void SinParser::skip_whitespace() {
+    skip_chars(whitespace);
+}
+
+std::string SinParser::read_till_char(const std::set<char> &chars) {
+    std::string res;
+    while (!ss.eof()) {
+        char ch = ss.peek();
+        if (chars.contains(ch)) {
+            break;
+        }
+        else {
+            res += ch;
+            get_char();
+        }
+    }
+    return res;
+}
+
+static bool char_is_alpha(char ch) {
+    if ((ch >= 'a') && (ch <= 'z')) {
+        return true;
+    }
+    if ((ch >= 'A') && (ch <= 'Z')) {
+        return true;
+    }
+    return false;
+}
+
+std::string SinParser::read_var_name() {
+    skip_whitespace();
+    if (ss.eof()) {
+        error += "\nReading variable name: EOF at line: " + std::to_string(line_number);
+        return {};
+    }
+    int ch = ss.peek();
+    if (ch == '[') {
+        get_char(); // [
+        auto res = read_till_char({']'});
+        get_char(); // ]
+        return res;
+    }
+    else if (ch == '.') {
+        get_char(); // .
+        return read_till_char(whitespace_or_colon);
+    }
+    else {
+        error += std::string("\nReading variable name: unexpected char '") + (char)ch + "' at line: " + std::to_string(line_number);
+        return {};
+    }
+}
+
+std::string SinParser::read_var_type() {
+    skip_whitespace();
+    if (ss.eof()) {
+        error += "\nReading variable type: EOF at line: " + std::to_string(line_number);
+        return {};
+    }
+    return read_till_char(whitespace);
+}
+
+std::string SinParser::read_number() {
+    skip_whitespace();
+    auto delimiters = whitespace;
+    delimiters.insert(':');
+    delimiters.insert('}');
+    return read_till_char(delimiters);
+}
+
+Sin SinParser::read_sin_value() {
+    skip_whitespace();
+    if (ss.eof()) {
+        error += "\nEOF while reading SIN value at line " + std::to_string(line_number);
+        return {};
+    }
+
+    int ch = get_char();
+    if (ch == EOF) {
+        error += "\n':' expected but EOF encountered at line " + std::to_string(line_number);
+        return {};
+    }
+    if (ch != ':') {
+        error += std::string("\n':' expected but ") + (char)ch + " encountered at line " + std::to_string(line_number);
+        return {};
+    }
+    skip_whitespace();
+
+    ch = ss.peek();
+    if (ch == EOF) {
+        error += "\nUnexpected EOF encountered at line " + std::to_string(line_number);
+        return {};
+    }
+    else if (ch == '\"') {
+        // TODO : read string
+        error += "\nStrings not implemented yet";
+        return {};
+    }
+    else if (char_is_alpha(ch)) {
+        std::string sin_type = read_var_type();
+        skip_whitespace();
+        std::string sin_value = read_number();
+
+        if (false) {
+            // dummy clause to use following macro
+        }
+#define PARSE_SMALL_INT(SIN_TYPE, STANDARD_TYPE)            \
+        else if (sin_type == #SIN_TYPE) {                   \
+            try {                                           \
+                STANDARD_TYPE value = std::stoi(sin_value); \
+                return value;                               \
+            }                                               \
+            catch (std::invalid_argument &e) {              \
+                error += std::string("\n") + e.what() + ". Can't parse '" + sin_value + "' at line " + std::to_string(line_number); \
+                return {};                                  \
+            }                                               \
+        }
+        PARSE_SMALL_INT(Int8,  int8_t)
+        PARSE_SMALL_INT(Int16, int16_t)
+        PARSE_SMALL_INT(Int32, int32_t)
+        else {
+            error += "\nUnsupported type: " + sin_type + " at line " + std::to_string(line_number);
+            return {};
+        }
+    }
+    else if (ch == '{') {
+        get_char();
+        skip_whitespace();
+        Sin sin;
+        while (true) {
+            int ch2 = ss.peek();
+            if (ch2 == EOF) {
+                error += "\nEOF at line " + std::to_string(line_number);
+                return sin;
+            }
+            else if (char_is_alpha(ch2) || (ch2 == '[')) {
+                auto name = read_var_name();
+                skip_whitespace();
+                Sin value = read_sin_value();
+                sin[name] = value;
+            }
+            else if (ch2 == '}') {
+                return sin;
+            }
+            else if (ch2 == '.') {
+                auto name = read_var_name();
+                skip_whitespace();
+                auto var  = read_sin_value();
+                skip_whitespace();
+                sin[name] = var;
+            }
+            else {
+                error += std::string("\nUnexpected characted '") + (char)ch2 + "' encountered at line " + std::to_string(line_number);
+                return sin;
+            }
+            skip_whitespace();
+        }
+        return sin;
+    }
+    else {
+        error += std::string("\nUnexpected characted '") + (char)ch + "' encountered at line " + std::to_string(line_number);
+        return {};
+    }
+}
+
+SinParser::SinParser(const std::string &str): ss(str) {
+    value = read_sin_value();
+}
+
+static void str_assert(const std::string &real, const std::string &expected, const std::string &descr = "error") {
+    if (expected != real) {
+        std::cerr << descr << ":\n'" << real << "'\n != \n'" << expected << "'\n";
+    }
+}
+
+// unit tests
+int main() {
+    SinParser sp1(":Int32\n1");
+    str_assert(sp1.error, "", "Test 1");
+    str_assert(sp1.value.type(), "Int32", "Test 1");
+    str_assert(std::to_string(sp1.value.asInt32()), "1", "Test1");
+
+    SinParser sp2(": {\n}\n");
+    str_assert(sp2.error, "", "Test 2");
+    str_assert(sp2.value.type(), "Object", "Test 2");
+
+    SinParser sp3(": {\n"
+                  "\t.element:{}}");
+    str_assert(sp3.error, "", "Test 3");
+    str_assert(sp3.value.type(), "Object", "Test 3");
+
+    SinParser sp4(": {\n"
+                  "\t.element:Int8 -33}");
+    str_assert(sp4.error, "", "Test 4");
+    str_assert(sp4.value.type(), "Object", "Test 4");
+    str_assert(sp4.value["element"].type(), "Int8", "Test 4");
+    str_assert(std::to_string(sp4.value["element"].asInt8()), "-33", "Test 4");
+
+    std::string str5 = ": {\n"
+                       "  .first: {\n"
+                       "    [second one]: {\n"
+                       "      [third one]: Int16\n"
+                       "      12345\n"
+                       "      [fourth one]: Int8\n"
+                       "      0\n"
+                       "    }\n"
+                       "  }\n"
+                       "}\n\n\n";
+    SinParser sp5(str5);
+    str_assert(sp5.error, "", "Test 5");
+    str_assert(sp5.value.type(), "Object", "Test 5");
+    str_assert(sp4.value["first"].type(), "Object", "Test 5");
+    str_assert(std::to_string(sp5.value["first"]["second one"]["third one"].asInt16()), "12345", "Test 5");
+    str_assert(std::to_string(sp5.value["first"]["second one"]["fourth one"].asInt8()), "0", "Test 5");
+}
